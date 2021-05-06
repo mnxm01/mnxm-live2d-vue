d7f7446991aaf63ffa64942632730428aa1c9833
wam <525987762@qq.com>
2021-05-06 12:10:36 +0800

    fix: 目录结构

---
+++ src/Live2d.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="live-2d">
+    <canvas :id="canvasId" :width="width" :height="height"></canvas>
+  </div>
+</template>
+
+<script>
+import "./libs/live2d.min.js";
+import Simple from "../libs/Simple.js";
+import uuidv4 from "uuid/v4";
+
+export default {
+  name: "Live2d",
+  props: {
+    width: {
+      default: 300,
+    },
+    height: {
+      default: 300,
+    },
+    onMove: {}, // 控制模型运动
+    modelData: {
+      // 模型导入的接口
+      type: Object,
+    },
+    index: {
+      type: Number,
+      default: 0,
+    },
+  },
+  data() {
+    return {
+      sp: null, // Simple对象实例
+      cvs: null, // 画布
+      canvasId: uuidv4(),
+    };
+  },
+  computed: {},
+  methods: {
+    move() {
+      this.sp.drawMove((model) => {
+        this.onMove(model);
+      });
+    },
+    liveLoad(simple, canvas) {
+      var that = this;
+      // Live2Dの初期化
+      window.Live2D.init();
+
+      // コンテキストを失ったとき(当我失去上下文)
+      canvas.addEventListener(
+        "webglcontextlost",
+        function (e) {
+          simple.myerror("context lost");
+          that.loadLive2DCompleted = false;
+          that.initLive2DCompleted = false;
+
+          var cancelAnimationFrame =
+            window.cancelAnimationFrame || window.mozCancelAnimationFrame;
+          cancelAnimationFrame(that.requestID); //アニメーションを停止(停止动画)
+
+          e.preventDefault();
+        },
+        false
+      );
+
+      // コンテキストが復元されたとき(当上下文恢复时)
+      canvas.addEventListener(
+        "webglcontextrestored",
+        function () {
+          simple.myerror("webglcontext restored");
+          simple.initLoop(canvas);
+        },
+        false
+      );
+
+      // Init and start Loop
+      simple.initLoop(canvas, this.index);
+    },
+  },
+  mounted() {
+    // 获得canvas
+    this.cvs = document.getElementById(this.canvasId);
+    // 配置live2d对象
+    this.sp = new Simple(
+      Object.assign(
+        {
+          type: "Live2D Model Setting",
+        },
+        this.modelData
+      )
+    );
+    // 将两者初始化
+    this.liveLoad(this.sp, this.cvs);
+
+    // this.moveSample();
+    this.move();
+  },
+};
+</script>
---
+++ src/index.js
@@ -0,0 +1,3 @@
+import Live2d from './Live2d.vue'
+
+export default Live2d;
